25d253f9b73f49c05d2da5c336706baaf6dd086e
Stephen Salve Doliente, Ph.D. <ssdoliente@gmail.com>
2026-05-06 09:47:15 +0100

    Remove unused variables

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -8,7 +8,7 @@ input_file = open('./eva-data.json', 'r')
 output_file = open('./eva-data.csv','w')
 graph_file = './cumulative_eva_graph.png'
 
-fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
+
 
 data=[]
 
